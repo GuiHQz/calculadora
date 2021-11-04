dc2c26f8ab13a56d5c2c437dedc599c9d00b9b44
Guilherme Queiroz <guilher.queiroz@gmail.com>
2021-11-04 15:09:39 -0300

    finalizado

--- src/features/calculator/soma.tsx
+++ src/features/calculator/soma.tsx
@@ -1,12 +1,17 @@
-import React,{useState} from "react";
+import React from "react";
 import './soma.css'
 
 export const Soma: React.FC = () => {
 
     const operacao = () => {
-        
-    }
+        const numero1 = document.getElementById("n1") as HTMLInputElement
+        const numero2 = document.getElementById("n2") as HTMLInputElement
+        const conta = parseInt(numero1.value) + parseInt(numero2.value)
+        const resultadoConta = document.getElementById("numeroTela") as HTMLParagraphElement
+        resultadoConta.innerHTML = conta.toString()
 
+    }
+    console.log(operacao)
     return (
         <div className="App">
             <h1>Calculadora</h1>
@@ -15,6 +20,9 @@ export const Soma: React.FC = () => {
                 <input id="n2" type="number" />
                 <div className="botao">
                     <button onClick={operacao}>+</button>
+                </div>
+                <div className="label">
+                    <label id="numeroTela"> </label>
                 </div>
             </div>
         </div>
